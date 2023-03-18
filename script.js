c9367af35879f3cd85a9790e867325b052894427
Jekko98 <jekko1998@gmail.com>
2023-03-18 10:56:40 +0700

    commit1

--- script.js
+++ script.js
@@ -14,4 +14,42 @@
 
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+    
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { // пока пользователь вводит строковое значение или 0 или ничего не вводит
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); // вопрос будет повторятся
+    }
+}
+
+start();
+
+function rememberMyFilms() {
+    for (let i=0; i<2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('Насколько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");    
+    }    
+}
